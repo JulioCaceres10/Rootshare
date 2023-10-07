9a73dad40b4c60d52fe8c7cd673953e4e51b1fc8
Julio Cesar Romaña Caceres <66482069+JulioCaceres10@users.noreply.github.com>
2023-10-07 00:58:10 -0500

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -101,7 +101,7 @@ const routes = [
 
 const router = createRouter({
   mode: '',
-  history: createWebHistory(process.env.BASE_URL),
+  history: createWebHistory(),
   routes,
 });
 router.beforeEach(async (to, from, next) => {
