62c1e34ea8bfe03b6c03a0fd7d0e51b6a291f779
Julio Cesar Romaña Caceres <66482069+JulioCaceres10@users.noreply.github.com>
2023-10-07 01:58:38 -0500

    Update NavbarAuth.vue

--- src/components/auth/NavbarAuth.vue
+++ src/components/auth/NavbarAuth.vue
@@ -1,11 +1,11 @@
 <template>
     <v-app-bar elevation="0">
 
-        <v-app-bar-title class="text-center">
+        <div class="text-center">
             <router-link to="/" style="height: 400px; ">
                 <v-img class="logoNavAuth" src="@/assets/IMG/casaModerna-1.jpg" style="height: 40px; display: inline-flex;"></v-img>
             </router-link>
-        </v-app-bar-title>
+        </div>
 
     </v-app-bar>
 </template>
