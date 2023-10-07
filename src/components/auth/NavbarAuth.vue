513a8ef6ce37068739ab49abf4382fb5aa17b606
Julio Cesar Romaña Caceres <66482069+JulioCaceres10@users.noreply.github.com>
2023-10-07 00:21:57 -0500

    Update NavbarAuth.vue

--- src/components/auth/NavbarAuth.vue
+++ src/components/auth/NavbarAuth.vue
@@ -3,7 +3,7 @@
 
         <v-app-bar-title class="text-center">
             <router-link to="/" style="height: 400px; ">
-                <v-img class="logoNavAuth" src="../../assets/IMG/logo.png" style="height: 40px; display: inline-flex;"></v-img>
+                <v-img class="logoNavAuth" src="../../assets/logo.png" style="height: 40px; display: inline-flex;"></v-img>
             </router-link>
         </v-app-bar-title>
 
