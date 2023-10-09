154fe24a2f056f8a41c49f7967665bab36b16d42
Julio Cesar Romaña Caceres <66482069+JulioCaceres10@users.noreply.github.com>
2023-10-08 19:01:32 -0500

    Update authUser.js

--- middleware/authUser.js
+++ middleware/authUser.js
@@ -3,7 +3,7 @@ import User from '../models/User.js';
 
 const authUser = async (req, res, next) => {
     // identificar si existe Token
-    const token = req.header('authToken')
+    const token = res.cookies
     console.log(token)
     
     
